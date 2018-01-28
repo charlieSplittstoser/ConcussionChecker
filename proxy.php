d0041af559c226f531c06d5351ad6c577cb4bd47
Charlie Splittstoser <char.split@gmail.com>
2018-01-28 13:17:24 -0600

    Cleanup html

--- proxy.php
+++ proxy.php
@@ -1,26 +1,30 @@
 <?php
+
 header("Access-Control-Allow-Origin: *");
 
-$c = $_GET['a'];
+/* Key, Latitude, Longitude */
+//$c = $_GET['a'];
 $lat = $_GET['lat'];
 $lng = $_GET['lng'];
-if($c == "pjm") {
-    $url = "https://maps.googleapis.com/maps/api/place/textsearch/json?query=Closest+hospital&location=" . $lat . "," . $lng . "&type=hospital&key=AIzaSyAeuPWQF7BynD5kceMl59kFsUKQ8I7mPTM";
-} else {
-    $url = "https://maps.googleapis.com/maps/api/place/textsearch/json?query=Closest+hospital&location=" . $lat . "," . $lng . "&type=hospital&key=AIzaSyAeuPWQF7BynD5kceMl59kFsUKQ8I7mPTM";
-}
 
+//if($c == "pjm") {
+    $url = "https://maps.googleapis.com/maps/api/place/textsearch/json?query=Closest+hospital&location=" . $lat . "," . $lng . "&type=hospital&key=AIzaSyAeuPWQF7BynD5kceMl59kFsUKQ8I7mPTM";
+/*} else {
+    $url = "https://maps.googleapis.com/maps/api/place/textsearch/json?query=Closest+hospital&location=" . $lat . "," . $lng . "&type=hospital&key=AIzaSyAeuPWQF7BynD5kceMl59kFsUKQ8I7mPTM";
+}*/
+
+/* Open request */
 $handle = fopen($url, "r");
 
+/* Handle request and output the response for the client to read */
 if ($handle) {
     while (!feof($handle)) {
         $buffer = fgets($handle, 4096);
         echo $buffer;
     }
 
+    /* Close the request handle */
     fclose($handle);
 }
-
-
 
 ?>
