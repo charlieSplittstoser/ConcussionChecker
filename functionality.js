e8bd5eed794c0e4a34da97f4199ed43dd7143d70
Charlie Splittstoser <char.split@gmail.com>
2018-01-28 00:03:18 -0600

    Update

--- functionality.js
+++ functionality.js
@@ -53,15 +53,16 @@ function handleLocationError(browserHasGeolocation, infoWindow, pos) {
 
   function calcSymptomNumber() {
         var checkboxes = [];
-
+        $('#checkbox-'+i).Class = "asda"
         var numberOfCheckboxes = 15;
 
         // get all checkbox elemetns
-        // for(i = 0; i < numberOfCheckboxes; i++){
-        //     var num = i + 1;
+         for(var i = 0; i < numberOfCheckboxes; i++){
+            var num = i + 1;
         //     console.log(num);
-        //     numberOfCheckboxes[i] = document.getElementById("checkbox-" + num).setAttribute("class", "test");
-        // }
-      document.getElementById("checkbox-1").setAttribute("class", "test");
+        $('#badge'+num).addClass('test');
+       //document.getElementById("checkbox-" + num).setAttribute("class", "test");
+         }
+      //document.getElementById("checkbox-1").setAttribute("class", "test");
   }
 
