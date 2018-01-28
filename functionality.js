65125ff5b866e41aa64c809a08a4bf421a71c887
Charlie Splittstoser <char.split@gmail.com>
2018-01-28 06:17:10 -0600

    Updating

--- functionality.js
+++ functionality.js
@@ -1,57 +1,12 @@
 var map, infoWindow;
 
-
-var symptomsarr = ["headache", "loss of consciousness","Confusion or disorientation","Lasting or recurrent dizziness",
-
-/* Has the map already been initiated? */
-var initiated = false;
-
-function loadMap() {
-    if (initiated == false) {
-        getLocation();
-        initiated = true;
-    }
-}
-
+/*
 var symptomssarr = ["headache", "loss of consciousness","Confusion or disorientation","Lasting or recurrent dizziness",
 "Difficulty recognizing people or places","Confusion or disorientation",
 "Changes in behavior/ irritability", "Repeated vomiting/nausea","Blurred Vision",
 "Change in eating or sleeping patterns","Loss of balance/unsteady walking","sensitivity to light and noise",
 "dilated pupils","Concentration and memory complaints"]; // array of sysmptoms' names
-
-var priorityarr = [8.85,8.0,7.5,7.88,6.92,7.5,3.85,7.73,4.0,4.5,5.58,5.77,3.27,8.27]; // corresponding priorities
-
-
-symptomsarr = ["headache",
-"loss of consciousness",
-"Confusion or disorientation",
-"Lasting or recurrent dizziness",
-"Difficulty recognizing people or places",
-"Ringing in ear",
-"Changes in behavior/ irritability", 
-"Repeated vomiting/nausea",
-"Blurred Vision",
-"Change in eating or sleeping patterns",
-"Loss of balance/unsteady walking",
-"sensitivity to light and noise",
-"dilated pupils",
-"Concentration and memory complaints"] // array of sysmptoms' names
-
-symptoms = []
-
-function SymptomClass(symptom, priority) { // class to store name priorty and if it was checked or not (capatalized S and c)
-  this.symptom = symptom
-  this.priority = priority;
-  this.checked = false;
-}
-
-var finalarr = []; // array to story classes we make
-var i = 0;
-while (symptomsarr[i]) { //make each class for each symptom
-  var symptomClass = new SymptomClass(symptomsarr[i],priorityarr[i]);
-  finalarr[i] = symptomClass;
-  i++;
-}
+*/
 
 function getHospitals() {
     // Try HTML5 geolocation.
@@ -83,12 +38,13 @@ function getHospitals() {
         xhttp.send();
 
       }, function() {
-        handleLocationError(true, infoWindow, map.getCenter());
+        //handleLocationError(true, infoWindow, map.getCenter());
+        alert("Error");
       });
     } else {
       // Browser doesn't support Geolocation
-
-      handleLocationError(false, infoWindow, map.getCenter());
+      alert("Error");
+      //handleLocationError(false, infoWindow, map.getCenter());
     }
 }
 
@@ -101,19 +57,38 @@ function handleLocationError(browserHasGeolocation, infoWindow, pos) {
     infoWindow.open(map);
   }
 
-  function calcSymptomNumber() {
+function calcSymptomNumber() {
+      getHospitals();
       var priorityArr = [8.85,8.0,7.5,7.88,6.92,4.0,3.85,7.73,4.0,4.5,5.58,5.77,3.27,8.27];
 
         var numberOfCheckboxes = 14;
         var badge = 'badge';
+        var total = 0;
+        var sum = 0;
          for (var i = 0; i < numberOfCheckboxes; i++){
             var num = i + 1;
         //     console.log(num);
              if(document.getElementById(badge + num).classList.contains("checked")) {
                 console.log(num);
+                total = total + priorityArr[i];
              }
+             sum = sum+ priorityArr[i]
+
        //document.getElementById("badge" + num).setAttribute("class", "test");
          }
+         result = "";
+         if (total >= 70) {
+          result = "High Risk"
+         }
+         else if (total>=35) {
+          result = "Intermidate Risk"
+         }
+         else{
+          result = "Low Risk"
+         }
+         document.getElementById("total").innerHTML = result;
+         console.log(total);
+         console.log(sum);
       // //document.getElementById("checkbox-1").setAttribute("class", "test");
   }
 
